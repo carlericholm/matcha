786e0f54bbd84d1a7b976aa813da8a1ef8008855
Maxime Imhoff <mimhoff@student.42.fr>
2018-10-30 13:30:23 +0100

    aignin + microtime

--- back/signin.js
+++ back/signin.js
@@ -14,27 +14,29 @@ router.post("/", function(req, res) {
 	con.query(sql, [login], function(err, result) {
 		if (result.length > 0)
 		{
-			if (hash.verify(password, result[0].password) == true)
+			if (result[0].active == 1)
 			{
-				//initialiser variable de session pour le login
-				req.log(login);
-				var sql = "SELECT * FROM pics WHERE login = ?";
-				con.query(sql, [req.session.log], function(err, result) {
-					var pics = result;
-					var sql = "SELECT * FROM users WHERE login = ?";
-					con.query(sql, [req.session.log], function(err, result) {
-						var users = result;
-						var sql = "SELECT * FROM tags WHERE login = ?";
+				if (hash.verify(password, result[0].password) == true)
+				{
+						//initialiser variable de session pour le login
+						req.log(login);
+						var sql = "SELECT * FROM pics WHERE login = ?";
 						con.query(sql, [req.session.log], function(err, result) {
-							var tags = result;
-							Object.keys(users[0]).map(function(key) {
-								if (typeof users[0][key] === "string") {
-									users[0][key] = tools.casseCouilles(users[0][key]);
-								}
-							});
-							res.render("index", {result: pics[0], info: users[0], tags: tags});
+							var pics = result;
+							var sql = "SELECT * FROM users WHERE login = ?";
+							con.query(sql, [req.session.log], function(err, result) {
+								var users = result;
+								var sql = "SELECT * FROM tags WHERE login = ?";
+								con.query(sql, [req.session.log], function(err, result) {
+									var tags = result;
+									Object.keys(users[0]).map(function(key) {
+										if (typeof users[0][key] === "string") {
+											users[0][key] = tools.casseCouilles(users[0][key]);
+										}
+									});
+									res.render("index", {result: pics[0], info: users[0], tags: tags});
+								})
 						})
-					})
 				})
 			}
 			else
@@ -42,6 +44,11 @@ router.post("/", function(req, res) {
 				req.flash("wrongPass", "Mot de passe incorrect");
 				res.redirect("signIn");
 			}
+		}
+		else {
+			req.flash("activate", "Activez votre compte pour vous connecter");
+			res.redirect("signIn");
+		}
 		}
 		else
 		{
